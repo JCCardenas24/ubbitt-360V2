72d1b9f0d4ab3a7f66ae2e9d7ad34912e190d318
Christian Del Rio <christian.delrio@arkdiasolutions.com>
2021-08-27 15:38:55 -0500

    agrega la página de términos y condiciones, en blanco

---
+++ controllers/TermsAndConditionsController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\controllers;
+
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+
+class TermsAndConditionsController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'actions' => ['index'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'index' => ['get'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
--- views/layouts/_main_footer.php
+++ views/layouts/_main_footer.php
@@ -1,8 +1,15 @@
+<?php
+
+use yii\helpers\Url;
+/* @var $this yii\web\View */
+
+?>
 <footer class="main-footer">
     <div class="container">
         <hr>
         <p>2021 Ubbitt 360 v2.0 | <span><a href="./aviso-privacidad-ubbitt-360.php" target="_blank"> Aviso de
-                    privacidad</a></span> - <span><a href="./aviso-privacidad-ubbitt-360.php" target="_blank"> Términos
+                    privacidad</a></span> - <span><a href="<?= Url::toRoute(['terms-and-conditions/index'])  ?>"
+                    target="_blank"> Términos
                     y condiciones de uso de licencia</a></span></p>
     </div>
 </footer>
