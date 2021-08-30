cb821b464e9d031bbdddc3c5a6b28b147e0d3717
Christian Del Rio <christian.delrio@arkdiasolutions.com>
2021-08-30 13:43:42 -0500

    Actualiza menus y dashboard de cobranza para ajustarse a la maqueta y reusar algunos componentes

--- views/ubbitt-beyond/_menu.php
+++ views/ubbitt-beyond/_menu.php
@@ -1,9 +1,21 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Url;
+?>
 <ul class="nav nav-pills level_two" id="beyond-home" role="tablist">
     <li class="nav-item" role="presentation">
-        <a class="nav-link active" id="cobranza-home-tab" data-toggle="pill" href="#cobranza-home" role="tab"
-            aria-controls="cobranza-home" aria-selected="true">Cobranza</a>
+        <a class="nav-link <?= 'ubbitt-beyond/collection-dashboard' == Yii::$app->getRequest()->getPathInfo() ? 'active' : '' ?>"
+            id="cobranza-home-tab"
+            <?= 'ubbitt-beyond/collection-dashboard' == Yii::$app->getRequest()->getPathInfo() ? 'data-toggle="pill"' : '' ?>
+            href="<?= Url::toRoute(['ubbitt-beyond/collection-dashboard']) ?>" role="tab" aria-controls="cobranza-home"
+            aria-selected="true">Cobranza</a>
     </li>
     <li class="nav-item" role="presentation">
-        <a class="nav-link" id="renovacion-home-tab" href="dashboard-beyond-renovacion.php">Renovación</a>
+        <a class="nav-link <?= 'ubbitt-beyond/renewal-dashboard' == Yii::$app->getRequest()->getPathInfo() ? 'active' : '' ?>"
+            id="renovacion-home-tab"
+            <?= 'ubbitt-beyond/renewal-dashboard' == Yii::$app->getRequest()->getPathInfo() ? 'data-toggle="pill"' : '' ?>
+            href="<?= Url::toRoute(['ubbitt-beyond/renewal-dashboard']) ?>">Renovación</a>
     </li>
 </ul>
--- views/ubbitt-beyond/collection-dashboard.php
+++ views/ubbitt-beyond/collection-dashboard.php
@@ -1,38 +1,22 @@
 <?php
 
 /* @var $this yii\web\View */
-
-use yii\helpers\Url;
-use yii\web\View;
-
 $this->title = 'Ubbitt 360';
 \app\assets\ChartsAsset::register($this);
-$this->registerJsFIle('attets/js/beyond-cobranza-charts.js');
+$this->registerJsFIle('assets/js/beyond-cobranza-charts.js');
 ?>
 <div class="container container-beyond" id="dynamic-tabs">
-    <ul class="nav nav-pills level_one level-one-beyond" id="main_tabs_panels" role="tablist">
-        <li class="nav-item" role="presentation">
-            <a class="nav-link active" id="beyond_option-tab" data-toggle="pill" href="#beyond_option" role="tab"
-                aria-controls="beyond_option" aria-selected="false">Ubbitt Beyond</a>
-        </li>
-
-        <li class="nav-item" role="presentation">
-            <!-- <a class="nav-link" id="premium-tab" data-toggle="pill" href="#premium" role="tab"
-                            aria-controls="premium" aria-selected="false">Ubbitt Premium</a> -->
-            <a class="nav-link" id="premium-tab">Ubbitt Premium</a>
-        </li>
-        <li class="nav-item" role="presentation">
-            <a class="nav-link " id="freemium_option-tab"
-                href="<?= Url::toRoute(['ubbitt-freemium/dashboard']) ?>">Ubbitt Freemium</a>
-        </li>
-
-    </ul>
+    <?= $this->render('../_commons/_tabs/_plans.php') ?>
     <div class="tab-content" id="main_tabs_panelsContent">
         <div class="tab-pane fade show active" id="beyond_option" role="tabpanel" aria-labelledby="beyond_option-tab">
             <?= $this->render('_menu') ?>
-            <?= $this->render('collection-dashboard/_submenu') ?>
-            <?= $this->render('collection-dashboard/_content') ?>
+            <div class="tab-content" id="beyond-homeContent">
+                <div class="tab-pane fade show active" id="cobranza-home" role="tabpanel"
+                    aria-labelledby="cobranza-home-tab">
+                    <?= $this->render('collection-dashboard/_submenu') ?>
+                    <?= $this->render('collection-dashboard/_content') ?>
+                </div>
+            </div>
         </div>
     </div>
-
 </div>
