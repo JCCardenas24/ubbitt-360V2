7b05d366cddd1564da309c05d04c4e161e209acf
Christian Del Rio <christian.delrio@ubbitt.com>
2021-09-02 22:32:14 -0500

    Agrega modelo de BD para almacenar los datos de la gráfica de resumen de Ubbitt Freemium

---
+++ models/db/FreemiumSummary.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace app\models\db;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the  class for table "freemium_summary".
+ *
+ * @property date $uploadDate
+ * @property date $date
+ * @property integer $leads
+ * @property integer $calls
+ * @property integer $sales
+ * @property integer $collected
+ *
+ */
+class FreemiumSummary extends ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'freemium_summary';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['upload_date', 'date', 'leads', 'calls', 'sales', 'collected'], 'required'],
+            [['leads', 'calls', 'sales', 'collected'], 'integer'],
+            [['upload_date', 'date'], 'date', 'format' => 'php:Y-m-d']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'upload_date' => 'Fecha de carga',
+            'date' => 'Fecha',
+            'leads' => 'Leads',
+            'calls' => 'Llamadas',
+            'sales' => 'Ventas',
+            'collected' => 'Cobrado',
+        ];
+    }
+
+    public function beforeSave($insert)
+    {
+        $this->upload_date = Yii::$app->formatter->asDatetime(strtotime($this->upload_date), "php:Y-m-d");
+        return parent::beforeSave($insert);
+    }
+
+    public function getUploadDate()
+    {
+        return $this->upload_date;
+    }
+
+    public function setUploadDate($uploadDate)
+    {
+        $this->upload_date = $uploadDate;
+    }
+
+    public function findByDate()
+    {
+        return self::find()->where(['date' => $this->date])->one();
+    }
+}
