8151ccf093800c894f28cf49a1d391ac66977c98
Christian Del Rio <christian.delrio@ubbitt.com>
2021-09-07 16:21:16 -0500

    Agrega DID's de beyond cobranza y beyond renovación

--- config/params.php
+++ config/params.php
@@ -2,5 +2,7 @@
 
 return [
     'itemsPerPage' => 10,
-    'ubbitt_freemium_did' => '5588547978'
+    'ubbitt_freemium_did' => '5588547978',
+    'ubbitt_beyond_collection_did' => '5589571959',
+    'ubbitt_beyond_renewal_did' => '5589505362',
 ];
