35d0cfacd99d702df7e089995e4af2944cc3da20
Christian Del Rio <christian.delrio@ubbitt.com>
2021-09-07 10:23:42 -0500

    Usa los tabs genéricos en la pantalla de freemium y marca el tab activo de acuerdo al controlador actual

--- views/_commons/_tabs/_plans.php
+++ views/_commons/_tabs/_plans.php
@@ -7,9 +7,9 @@ use yii\helpers\Url;
 <ul class="nav nav-pills level_one level-one-beyond" id="main_tabs_panels" role="tablist">
     <?php if (in_array('menu_ubbitt_beyond', Yii::$app->session->get("userPermissions"))) { ?>
     <li class="nav-item" role="presentation">
-        <a class="nav-link active" id="beyond_option-tab" data-toggle="pill"
-            href="<?= Url::toRoute(['ubbitt-beyond/renewal-dashboard']) ?>" role="tab" aria-controls="beyond_option"
-            aria-selected="false">Ubbitt Beyond</a>
+        <a class="nav-link <?= Yii::$app->controller->id == 'ubbitt-beyond' ? 'active' : '' ?>" id="beyond_option-tab"
+            data-toggle="pill" href="<?= Url::toRoute(['ubbitt-beyond/renewal-dashboard']) ?>" role="tab"
+            aria-controls="beyond_option" aria-selected="false">Ubbitt Beyond</a>
     </li>
     <?php } ?>
     <?php
@@ -18,14 +18,16 @@ use yii\helpers\Url;
     <li class="nav-item" role="presentation">
         <!-- <a class="nav-link" id="premium-tab" data-toggle="pill" href="#premium" role="tab"
                             aria-controls="premium" aria-selected="false">Ubbitt Premium</a> -->
-        <a class="nav-link" id="premium-tab">Ubbitt Premium</a>
+        <a class="nav-link <?= Yii::$app->controller->id == 'ubbitt-premium' ? 'active' : '' ?>" id="premium-tab">Ubbitt
+            Premium</a>
     </li>
     <?php } ?>
     <?php
     if (in_array('menu_ubbitt_freemium', Yii::$app->session->get("userPermissions"))) {
     ?>
     <li class="nav-item" role="presentation">
-        <a class="nav-link " id="freemium_option-tab" href="<?= Url::toRoute(['ubbitt-freemium/dashboard']) ?>">Ubbitt
+        <a class="nav-link <?= Yii::$app->controller->id == 'ubbitt-freemium' ? 'active' : '' ?>"
+            id="freemium_option-tab" href="<?= Url::toRoute(['ubbitt-freemium/dashboard']) ?>">Ubbitt
             Freemium</a>
     </li>
     <?php } ?>
--- views/ubbitt-freemium/dashboard.php
+++ views/ubbitt-freemium/dashboard.php
@@ -2,29 +2,14 @@
 
 /* @var $this yii\web\View */
 
-use yii\helpers\Url;
 use yii\web\View;
 
 $this->title = 'Ubbitt 360';
 \app\assets\ChartsAsset::register($this);
 $this->registerJsFile('@web/assets/js/views/ubbitt-freemium/freemium-dashboard.js', ['position' => View::POS_END, 'depends' => [\app\assets\ChartsAsset::class]]);
 ?>
-<div class="container" id="dynamic-tabs">
-    <ul class="nav nav-pills level_one" id="main_tabs_panels" role="tablist">
-        <li class="nav-item" role="presentation">
-            <a class="nav-link active" id="freemium_option-tab" data-toggle="pill" href="#freemium_option" role="tab"
-                aria-controls="freemium_option" aria-selected="true">Ubbitt Freemium</a>
-        </li>
-        <li class="nav-item" role="presentation">
-            <!-- <a class="nav-link" id="premium-tab" data-toggle="pill" href="#premium" role="tab"
-                            aria-controls="premium" aria-selected="false">Ubbitt Premium</a> -->
-            <a class="nav-link" id="premium-tab">Ubbitt Premium</a>
-        </li>
-        <li class="nav-item" role="presentation">
-            <a class="nav-link" id="beyond_option-tab"
-                href="<?= Url::toRoute(['ubbitt-beyond/collection-dashboard']) ?>">Ubbitt Beyond</a>
-        </li>
-    </ul>
+<div class="container container-beyond" id="dynamic-tabs">
+    <?= $this->render('../_commons/_tabs/_plans.php') ?>
     <div class="tab-content" id="main_tabs_panelsContent">
         <div class="tab-pane fade show active" id="freemium_option" role="tabpanel"
             aria-labelledby="freemium_option-tab">
