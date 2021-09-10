c6dfb5a498d8720a5497249f0ce2bcd997ddcbc8
Christian Del Rio <christian.delrio@ubbitt.com>
2021-09-10 16:37:07 -0500

    Aumenta el tiempo que dura el mensaje toast en la vista

--- views/_commons/_widgets/_toast.php
+++ views/_commons/_widgets/_toast.php
@@ -1,5 +1,5 @@
 <div class="toast_wrapper" style="z-index: 5;">
-    <div id="toast-alert" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true" data-delay="3000">
+    <div id="toast-alert" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true" data-delay="9000">
         <div class="toast-body">
             <p class="m-0"></p>
         </div>
