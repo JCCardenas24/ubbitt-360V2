5d56a3b1102efb29f19673ff0b0e403cc75e85cc
Christian Del Rio <christian.delrio@ubbitt.com>
2021-09-14 14:19:36 -0500

    Cambia el z-index del toast para que se muestre sobre los modales

--- views/_commons/_widgets/_toast.php
+++ views/_commons/_widgets/_toast.php
@@ -1,4 +1,4 @@
-<div class="toast_wrapper" style="z-index: 5;">
+<div class="toast_wrapper" style="z-index: 1500;">
     <div id="toast-alert" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true" data-delay="9000">
         <div class="toast-body">
             <p class="m-0"></p>
