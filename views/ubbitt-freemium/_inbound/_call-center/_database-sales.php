e3752b5cb011103d42d7ac5327910836b3bd9a4f
Christian Del Rio <christian.delrio@ubbitt.com>
2021-10-04 16:54:33 -0500

    Corrige los ids de los selectores de fechas

--- views/ubbitt-freemium/_inbound/_call-center/_database-sales.php
+++ views/ubbitt-freemium/_inbound/_call-center/_database-sales.php
@@ -1,7 +1,7 @@
 <div class="d-flex wid-100 justify-content-end">
     <div class="col-4 d-flex justify-content-between">
         <div class="form-group wid-100 d-flex m-0">
-            <div id="freemium-calls-database-date-range" class="range-pick">
+            <div id="freemium-sales-database-date-range" class="range-pick">
                 <i class="fa fa-calendar"></i>&nbsp;
                 <span class="text-date"></span>&nbsp;&nbsp;<i class="fa fa-caret-down"></i>
             </div>
