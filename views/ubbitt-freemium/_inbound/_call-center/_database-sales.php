ae08ced8dfdb8b8e4972c3817b7b7337956004ab
Christian Del Rio <christian.delrio@ubbitt.com>
2021-10-07 12:07:11 -0500

    Actualización por cambios en maqueta

--- views/ubbitt-freemium/_inbound/_call-center/_database-sales.php
+++ views/ubbitt-freemium/_inbound/_call-center/_database-sales.php
@@ -1,13 +1,20 @@
-<div class="d-flex wid-100 justify-content-end">
-    <div class="col-4 d-flex justify-content-between">
-        <div class="form-group wid-100 d-flex m-0">
-            <div id="freemium-sales-database-date-range" class="range-pick">
-                <i class="fa fa-calendar"></i>&nbsp;
-                <span class="text-date"></span>&nbsp;&nbsp;<i class="fa fa-caret-down"></i>
-            </div>
+<div class="d-flex justify-content-end mb-20">
+    <div class="input-group input_group_search mr-5">
+        <input id="search-term" type="text" class="form-control" placeholder="Buscar">
+        <div class="input-group-append">
+            <button class="btn btn-secondary" type="button" onclick="onFilterSalesDatabase()">
+                <i class="fa fa-search"></i>
+            </button>
         </div>
     </div>
-    <!-- <a href="#" class="pdf_button mr-5"><i class="icon-download_xls"></i></a> -->
+    <div id="freemium-sales-database-date-range" class="range-pick range_dates_width mr-5">
+        <i class="fa fa-calendar"></i>&nbsp;
+        <span class="text-date"></span>&nbsp;&nbsp;<i class="fa fa-caret-down"></i>
+    </div>
+    <!-- <a href="./assets/reportes/prueba.xlsx" class="pdf_button mr-5" download><i class="icon-download_xls c-gray"
+            aria-hidden="true"></i></a> -->
+    <a href="./assets/reportes/prueba.xlsx" class="pdf_button" download><i class="fa fa-download"
+            aria-hidden="true"></i></a>
 </div>
 <br>
 <?= $this->render('_database-sales/_table') ?>
