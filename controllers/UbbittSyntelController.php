e7dee9b5b073235fa793efb98703b8de73e47ee7
Andrés Escobar <andres.escobar@ubbitt.com>
2021-10-29 14:37:38 -0500

    AGREGAR SERVICIO PARA ACTUALIZAR SYNTELCALLS DE LEAD

--- controllers/UbbittSyntelController.php
+++ controllers/UbbittSyntelController.php
@@ -21,6 +21,7 @@ class UbbittSyntelController extends Controller
             'actions' => [
                 'last-call' => ['get'],
                 'store-calls' => ['post'],
+                'update-calls' => ['post'],
             ],
         ];
 
@@ -58,6 +59,30 @@ class UbbittSyntelController extends Controller
         $last_call =SyntelCallInfo::find()->orderBy(['pk_call'=> SORT_DESC])->one();
 
         return $last_call != null ? $last_call->pk_call : null;
+    }
+    
+    public function actionUpdateCalls() {
+        $data = $this->request->post();
+        $lead_id = $data['lead_id'];
+        $calls = json_decode($data['calls'], true, 512, JSON_UNESCAPED_UNICODE);
+
+        foreach ($calls as $call) {
+            $syntelCall = SyntelCallInfo::findOne(['pk_call' => $call['pk_call']]);
+            $syntelCall->callType = $call['call_type'];
+            $syntelCall->callPurpose = $call['call_purpose'];
+            $syntelCall->callStart = $call['call_start'];
+            $syntelCall->callEnd = $call['call_end'];
+            $syntelCall->trackerName = $call['tracker_name'];
+            $syntelCall->stepName = $call['step_name'];
+            $syntelCall->callpickerNumber = $call['troncal_number'];
+            $syntelCall->salesStatus = $call['sales_status'];
+            $syntelCall->collectedStatus = $call['collected_status'];
+            $syntelCall->amount = $call['amount'];
+            $syntelCall->fkLead = $call['fk_lead'];
+            $syntelCall->save();
+        }
+
+        return json_encode(['message' => "Calls updated: ".count($calls)." from lead: ".$lead_id]);
     }
 
 }
